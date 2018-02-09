7830f263ccd3e7f1cdce130625366caa880115aa
Bryan Martin <bmart847@gmail.com>
2018-02-09 13:33:30 -0500

    Added skeleton for node demo-1

---
+++ demo1_node.py
@@ -0,0 +1,27 @@
+"""
+This is the code that will simulate the host for our first demo.
+It will wait for a message from the node then broadcast it to
+the server.
+"""
+
+from networking import WebConnection
+from node import Node
+
+def main():
+    """ Create an object to represent this parking spot """
+    this_spot = Node(1, "192.168.2.2")
+    
+    """ Create an object to broadcoast updates to host """
+    mesh_connection = WebConnection("192.168.2.1", 1234)
+    
+    """ Create an object to recieve data from sensor """
+    # gpio_connection = 
+
+    while True:
+        """ Wait for updates from sensor """
+        
+        """ Broadcast to Host """
+        mesh_connection.transmit_object(this_spot)
+
+        
+main()
